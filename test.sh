b9fa8eced04ee687b67cf5dc3538d9c97e46a110
gonzalogacc <gonzalogacc@gmail.com>
2023-11-28 16:16:03 +0100

    script

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-psql -h ${POSTGRES_HOST} -U postgres -p ${POSTGRES_PORT} -l
+psql -h ${POSTGRES_HOST} -U postgres_user -l 
+psql -h ${POSTGRES_HOST} -d postgres_db -U postgres_user -c 'SELECT 1;'
